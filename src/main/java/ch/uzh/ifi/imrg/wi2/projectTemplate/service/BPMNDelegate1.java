925b4179a5b1dc8edd8139eb4a65728902514c68
Lawand97 <lawand.97@hotmail.com>
2018-10-30 22:11:21 +0100

    bpmn

---
+++ src/main/java/ch/uzh/ifi/imrg/wi2/projectTemplate/service/BPMNDelegate1.java
@@ -0,0 +1,20 @@
+package ch.uzh.ifi.imrg.wi2.projectTemplate.service;
+
+import java.util.Map;
+import javax.inject.Named;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+@Named("aServiceDelegate")
+public class BPMNDelegate1 implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution de) throws Exception {
+        Map<String, Object> camVariables = de.getVariables();
+        for(String k : camVariables.keySet()) {
+            System.out.println("camVariables["+k+"] = " + camVariables.get(k));
+        }
+        
+    }
+    
+}
